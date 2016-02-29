a7ea8c029508a35e2cc624cc6a02de625b8f689e
Everardo Guevara <kaz.msf@gmail.com>
2016-02-29 13:14:01 -0600

    Actualización

--- juegoDeMemoria.playground/Contents.swift
+++ juegoDeMemoria.playground/Contents.swift
@@ -14,14 +14,12 @@ for i in 0...100 {
         print("\(i) Bingo!!!")
     }
     
-    if (i != 0) {
-        if (i % 2 == 0 ) {
-            print("\(i) par")
-        } else {
-            print("\(i) impar")
-        }
+    if (i % 2 == 0 ) {
+        print("\(i) par")
+    } else {
+        print("\(i) impar")
     }
-        
+    
     if i >= 30 && i <= 40 {
         print("\(i) Viva Swift!!!")
     }
